d4613f675bea9a007a04d3f50ab73090d6096624
Brian Diaz <bidiaz101@gmail.com>
2021-09-14 17:53:15 -0500

    Progress

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+    useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(resp => resp.json())
+    .then(data => setQuestions(data))
+  }
+  , [])
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE"
+    })
+    .then(() => setQuestions(questions))
+    // setQuestions(questions.filter(question => question.id !== id))
+  }
+
+  function handleChange(id, correctIndex){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({
+        "correctIndex": parseInt(correctIndex)
+      })
+    })
+    .then(() => setQuestions(questions))
+  }
+
+  const questionItems = questions.map(questionObj => {
+    return <QuestionItem 
+      question={questionObj} 
+      key={questionObj.id} 
+      handleDelete={handleDelete}
+      handleChange={handleChange}
+    />
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
